3f16419b79dfdbbb23c7b226b393e0920a6661c1
Head Gardener <trashbin2019np@gmail.com>
2025-07-28 15:34:06 +0300

    gitignore, formatting

--- flake.nix
+++ flake.nix
@@ -8,32 +8,52 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { pkgs, lib, ... }: { packages = {
-        capt-print = pkgs.stdenv.mkDerivation {
-          name = "capt";
-          version = "0.1";
+      perSystem = { pkgs, lib, ... }: {
+        packages = rec {
+          capt-bin = pkgs.stdenv.mkDerivation {
+            name = "capt-bin";
+            version = "0.1";
 
-          src = ./.;
+            src = ./.;
 
-          buildPhase = ''
-            make capt
-          '';
+            buildPhase = ''
+              make capt
+            '';
 
-          installPhase = ''
-            mkdir -p $out/bin
-            cp capt $out/bin/
-            chmod +x $out/bin/capt
+            installPhase = ''
+              mkdir -p $out/bin
+              cp capt $out/bin/
+              chmod +x $out/bin/capt
+            '';
+          };
 
-            cp capt-print $out/bin/
-            substituteInPlace $out/bin/capt-print \
-              --replace /usr/bin/gs '${lib.getBin pkgs.ghostscript}'
-            chmod +x $out/bin/capt-print
+          default = pkgs.stdenv.mkDerivation {
+            name = "capt-canon-lbp-810";
+            version = "0.1";
 
-            mkdir -p $out/share/cups/model/canon/
-            cp ppd/Canon-LBP-810-capt.ppd $out/share/cups/model/canon/
-          '';
+            src = ./.;
+
+            buildPhase = ''
+              true
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp capt-print $out/bin/
+              substituteInPlace $out/bin/capt-print \
+                --replace /usr/bin/gs "${pkgs.ghostscript}/bin/gs" \
+                --replace /usr/bin/capt "${capt-bin}/bin/capt"
+              chmod +x $out/bin/capt-print
+
+              mkdir -p $out/share/cups/model/canon/
+              cp ppd/Canon-LBP-810-capt.ppd $out/share/cups/model/canon/
+              substituteInPlace $out/share/cups/model/canon/* \
+                --replace gs "${pkgs.ghostscript}/bin/gs" \
+                --replace "|capt" "|${capt-bin}/bin/capt"
+            '';
+          };
         };
-      }; };
+      };
 
       flake = { };
     };
